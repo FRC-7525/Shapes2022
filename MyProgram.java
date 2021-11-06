e89b3722799122e9b2695645bc7fa952a17c7dfc
Bogdan V. Bradu <bradubv@hotmail.com>
2021-11-06 12:06:08 -0500

    Added a program [does not compile]

---
+++ MyProgram.java
@@ -0,0 +1,6 @@
+public class MyProgram {
+    public static void MyProgram() {
+        Rectangle myFirstRectangle = new Rectangle();
+        System.out.println(myFirstRectangle.getPerimeter());
+    }
+}

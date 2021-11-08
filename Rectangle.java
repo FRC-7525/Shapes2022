35fbbdddb09e8c84bfc9fa212ced564e8d408137
bogdan.bradu <bradubv@hotmail.com>
2021-11-07 23:46:57 -0600

    Silly way to get a perimeter other than zero

--- Rectangle.java
+++ Rectangle.java
@@ -1,6 +1,6 @@
 public class Rectangle {
-    public double length;
-    public double width;
+    public double length = 3.0;
+    public double width = 4.0;
 
     public double getPerimeter() {
         return 2*(length + width);

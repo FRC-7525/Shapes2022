a26ccb875b4e910e5d77e17a5369157be0ba2410
bogdan.bradu <bradubv@hotmail.com>
2021-11-07 23:33:42 -0600

    First version that runs

--- MyProgram.java
+++ MyProgram.java
@@ -1,5 +1,5 @@
 public class MyProgram {
-    public static void MyProgram() {
+    public static void main(String[] args) {
         Rectangle myFirstRectangle = new Rectangle();
         System.out.println(myFirstRectangle.getPerimeter());
     }

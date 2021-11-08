12b3845d0ddca7ef30f0e23b5ecf5086631b7231
bogdan.bradu <bradubv@hotmail.com>
2021-11-08 00:09:30 -0600

    Code circle shape

--- MyProgram.java
+++ MyProgram.java
@@ -1,10 +1,14 @@
 public class MyProgram {
     public static void main(String[] args) {
         Rectangle myFirstRectangle = new Rectangle(3, 4);
-        System.out.println("myFirstRectangle Perimeter = " + myFirstRectangle.getPerimeter());
+        System.out.println("myFirstRectangle Perimeter = " + myFirstRectangle.getPerimeter() + "\n");
 
         Rectangle mySecondRectangle = new Rectangle(4,5);
         System.out.println("mySecondRectangle Perimeter = " + mySecondRectangle.getPerimeter());
-        System.out.println("mySecondRectangle Area = " + mySecondRectangle.getArea());
+        System.out.println("mySecondRectangle Area = " + mySecondRectangle.getArea() + "\n");
+
+        Circle myFirstCircle = new Circle(3);
+        System.out.println("myFirstCircle Perimeter = " + myFirstCircle.getPerimeter() );
+        System.out.println("myFirstCicle Area = " + myFirstCircle.getArea());
     }
 }

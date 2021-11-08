80f36d09186588e1eaf9ea476dcba850a780ad0a
bogdan.bradu <bradubv@hotmail.com>
2021-11-07 23:54:48 -0600

    Create second rectangle

--- MyProgram.java
+++ MyProgram.java
@@ -1,6 +1,9 @@
 public class MyProgram {
     public static void main(String[] args) {
         Rectangle myFirstRectangle = new Rectangle(3, 4);
-        System.out.println(myFirstRectangle.getPerimeter());
+        System.out.println("myFirstRectangle Perimeter = " + myFirstRectangle.getPerimeter());
+
+        Rectangle mySecondRectangle = new Rectangle(4,5);
+        System.out.println("mySecondRectangle Perimeter = " + mySecondRectangle.getPerimeter());
     }
 }

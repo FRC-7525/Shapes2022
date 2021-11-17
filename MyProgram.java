5f35fa2ae7306fafc7dbbac4db28935decf5d8bd
Team7525 <pioneers.robotics@gmail.com>
2021-11-17 16:09:50 -0600

    Everything inherets from shape

--- MyProgram.java
+++ MyProgram.java
@@ -1,18 +1,19 @@
 public class MyProgram {
     public static void main(String[] args) {
+        Shape[] shapes = new Shape[4];
+
         Rectangle myFirstRectangle = new Rectangle(3, 4);
-        System.out.println("myFirstRectangle Perimeter = " + myFirstRectangle.getPerimeter() + "\n");
-
         Rectangle mySecondRectangle = new Rectangle(4,5);
-        System.out.println("mySecondRectangle Perimeter = " + mySecondRectangle.getPerimeter());
-        System.out.println("mySecondRectangle Area = " + mySecondRectangle.getArea() + "\n");
-
-        Circle myFirstCircle = new Circle(3);
-        System.out.println("myFirstCircle Perimeter = " + myFirstCircle.getPerimeter() );
-        System.out.println("myFirstCicle Area = " + myFirstCircle.getArea());
-
-        Square myFirstSquare = new Square(1);
-        System.out.println("myFirstSquare Perimeter = " + myFirstSquare.getPerimeter());
-        System.out.println("myFirstSquare Area = " + myFirstSquare.getArea());
+        shapes[0]=myFirstRectangle;
+        shapes[1]=mySecondRectangle;
+        shapes[2]= new Circle(3);
+        shapes[3] = new Square(1);
+        
+        int i=0;
+        for (Shape r:shapes){
+            System.out.println("Shape " + i + " Perimeter" + r.getPerimeter());
+            System.out.println("Shape " + i + " Area = " + r.getArea() + "\n");
+            i++;
+        }
     }
 }
